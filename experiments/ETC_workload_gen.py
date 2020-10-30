ef6d94fdb20b0132ff2b43de25e04000480ed1f7
aarati-K <aarati.agc10@gmail.com>
2020-10-30 08:44:29 -0500

    Bug fixes in ETC workload generation

--- experiments/ETC_workload_gen.py
+++ experiments/ETC_workload_gen.py
@@ -3,13 +3,14 @@ import sys
 import time
 import redis
 import struct
+import random
 
 # Phase 1: warmup
 
 # 2.5 million keys to insert during warmup
 # They generate an rdb file of about 9GB
 warmup_filename = "/users/aarati_K/hdd/ETC/warmup"
-f = open(warmup_filename, "wb")
+f = open(warmup_filename, "w")
 num_kv_to_generate = 2500000
 keys_inserted = []
 batch_size = 100
@@ -21,7 +22,7 @@ try:
         rns = np.random.randint(0, sys.maxint, batch_size, dtype=np.int64)
         keys_inserted.extend(rns)
         for rn in rns:
-            f.write(struct.pack('>q', rn))
+            f.write("{}\n".format(str(rn)))
 except:
     pass
 end = time.time()
@@ -29,32 +30,35 @@ print "Time taken to generate warmup keys:", end-start, "seconds"
 print
 f.close()
 
-# Phase 2: ETC workload - Generate total 50M requests
+# Phase 2: ETC workload - Generate total 100M requests
 # 30:1 ratio of GET:SET
 # Batch size: 10? (review this choice later)
 # Same batch size for both GET and SET
 # SET requests require sampling of new keys
 # GET requests need to follow a zipfian, right now I use a bucketed zipfian
-num_requests_to_generate = 50003000
-num_set_requests = num_requests_to_generate/31 # 1.613 million
-num_get_requests = num_requests_to_generate - num_set_requests # 48.39 million
+num_requests_to_generate = 100006000
+num_set_requests = num_requests_to_generate/31 # 3.226 million
+num_get_requests = num_requests_to_generate - num_set_requests # 96.78 million
 batch_size = 10
 
 # approximate zipfian
 # There are 25000 buckets, each with a 100 keys initially (total 2.5M keys)
 # Dynamically allocate newly inserted keys to these buckets
-# Bucket size will become approx 150 by the end of the experiment
+# Bucket size will become approx 250 by the end of the experiment
 bucket_size = 100
 num_buckets = len(keys_inserted)/bucket_size
 random.shuffle(keys_inserted)
 buckets = []
 for i in range(num_buckets):
-    bucket = keys_inserted[i*bucket_size:((i+1)*bucket_size-1)]
+    bucket = keys_inserted[i*bucket_size:(i+1)*bucket_size]
     buckets.append(bucket)
 # Sanity check
 if num_buckets != len(buckets):
-    print "Something wrong with bucket generation, needed:", 
+    print "Something wrong with bucket generation, needed:", \
         num_buckets, "generated:", len(buckets)
+
+if len(buckets[0]) != bucket_size:
+    print "Bucket 0 doesn't match desired size"
 
 # Calculate probability of each bucket
 probabilities = [0]*num_buckets
@@ -67,14 +71,14 @@ for i in range(num_buckets):
 total = sum(probabilities)
 probabilities[0] += 1-total
 
-# We will have 100 iterations of approx 0.5 million requests each
-num_iterations = 100
+# We will have 200 iterations of approx 0.5 million requests each
+num_iterations = 200
 num_requests_per_iteration = num_requests_to_generate/num_iterations # 500030
 num_get_requests_per_iteration = num_get_requests/num_iterations # 483900
 
 # For each get request in an iteration choose a bucket
 # First calculate the frequency of occurence of each bucket 
-bucket_freq = []
+bucket_freq = [0]*num_buckets
 for i in range(num_buckets):
     bucket_freq[i] = int(num_get_requests_per_iteration*probabilities[i])
 total = sum(bucket_freq)
@@ -99,21 +103,21 @@ for i in range(100000):
 rlen = len(random_choice_array)
 
 insert_filename = "/users/aarati_K/hdd/ETC/insert"
-fetch_filename = "/users/aarati_K/hdd/ETC/insert"
-f1 = open(insert_filename, "wb")
-f2 = open(fetch_filename, "wb")
+fetch_filename = "/users/aarati_K/hdd/ETC/fetch"
+f1 = open(insert_filename, "w")
+f2 = open(fetch_filename, "w")
 
-# 100 * 1613 * 310 = 50003000 (50.003 million requests total)
-for i in range(100):
+# 200 * 1613 * 310 = 100006000 (100.006 million requests total)
+for i in range(num_iterations):
     for j in range(1613):
         # Batch size = 10, 31 batches of requests (310 requests total)
         # 1 batch (= 10) of SET requests
-        rns = np.randint(0, sys.maxint, batch_size, dtype=np.int64)
+        rns = np.random.randint(0, sys.maxint, batch_size, dtype=np.int64)
         for rn in rns:
-            f1.write(struct.pack('>q', rn))
+            f1.write("{}\n".format(str(rn)))
             rand_bucket = random_choice_array[rit] % num_buckets
             rit = (rit+1)%rlen
-            buckets[rand_bucket] = buckets[rand_bucket].append(rn)
+            buckets[rand_bucket].append(rn)
 
         # 30 batches (= 300) GET requests
         for k in range(300):
@@ -121,7 +125,7 @@ for i in range(100):
             bucket_len = len(buckets[bid])
             rand_bucket_pos = random_choice_array[rit]%bucket_len
             rand_key = buckets[bid][rand_bucket_pos]
-            f2.write(struct.pack('>q', rand_key))
+            f2.write("{}\n".format(str(rand_key)))
 
 f1.close()
 f2.close()
